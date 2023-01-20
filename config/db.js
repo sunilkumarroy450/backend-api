4af601ad7fbb95bc4a6e3a7397f704ffbc9a16c7
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-01-21 00:46:10 +0530

    updated atlas url

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const mongoose=require("mongoose")
+
 const connect=async ()=>{
     return mongoose.connect(process.env.MONGODB_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
     }).then(console.log("connected to mongodb")).catch(err=>console.log(err))
 }
+mongoose.set('strictQuery', false)
 module.exports=connect
